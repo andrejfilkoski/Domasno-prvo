a014252bc6e515d5f819f4df9384635e2f0377a0
Petar <blerinaazis@gmail.com>
2022-11-01 22:23:48 +0100

    Domasno prvo

---
+++ src/domasno1/Domasno1.java
@@ -0,0 +1,10 @@
+package domasnoprvo;
+
+public class Domasnoprvo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World OOP FIKT");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author filip
+ *
+ */
+module domasno1 {
+}
